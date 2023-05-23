05317f2285f35fa4ce4ed84c835749f369bc0762
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-05-23 13:31:47 +0000

    kadai3sign_in画面編集中]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,:sign_in, keys: [:name])
+  end
+
 end

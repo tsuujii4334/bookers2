4edf8f6884a306df76c95b7cb4b1f42a4622a993
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-05-25 13:01:42 +0000

    kadai3[投稿機能のviewfileを編集中]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    
+  end
+  
+  def index
+    
+  end
+  
   def show
+    
   end
 
   def edit
+    
+  end
+  
+  def update
+    
   end
 
-  def index
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/index'
-  get 'users/edit'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/index'
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => 'homes#about', as: 'about'
+  resources :books
+  resources :users, only: [:index, :show, :edit, :update]
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

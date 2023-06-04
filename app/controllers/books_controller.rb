40130377d538168e7a41f7fa400fcb3159a90ce4
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-06-04 16:16:54 +0000

    kadai3[useredit機能編集中]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new #新しくbookレコードを取得 
+    @book = Book.new #新しくbookレコードを取得
   end
-  
-  
+
+
   #投稿データの保存
   def create
     @book = Book.new(book_params)
@@ -11,15 +11,16 @@ class BooksController < ApplicationController
     @book.save
     redirect_to book_path(@book.id) #@book.id(新しく取得したbookレコード)のbookshowページに飛ぶ
   end
-  
+
   def index
-    @user = User.find(params[:id])
-    @book = Book.find(params[:id])
-    @books = book.all
+    @user = current_user
+    @book = Book.new(params[:id])
+    @books = Book.all
   end
-  
+
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new(book_params)
+    #bookshowでも@bookを使いたいがすでにbooknewで使っていいるためどう書いたらいいのかわからない
     @user = User.find(params[:id])
   end
 
@@ -27,22 +28,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id]) #そのままidが適用される？
   end
   
-  def update
-    
-  end
-  
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   #投稿データのストロングパラメータ
   private
-  
+
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
 
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,15 +16,17 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(current_user.id)
   end
-  
+
   def update
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     @user.update(user_params)
-    redirect_to user_path(@user.id)
+    redirect_to user_path(current_user.id)
   end
-  
-  
+
+
   private
-  
-  
+
+  def user_params
+    params.require(:user).parmit(:name, :profile_image, :introduction)  #なぜか消えない×　ルーティングエラーが
+  end
 end

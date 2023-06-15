28e8c215d05d081f804fc4974931a9a83921fba2
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-06-15 13:53:10 +0000

    kadai3[uershowレイアウト完成]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
+    
     @user = User.find(params[:id])
     @books = @user.books.all
     @newbook = Book.new
